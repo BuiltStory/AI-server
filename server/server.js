e454e7cbe63e97f3d2241f74fe642e6ee7be6927
Brian Alonso <bcalonso@builtstory.com>
2023-05-28 16:03:43 -0400

    /chat

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  basePath: "https://api.openai.com/v1/brian",
+  basePath: "https://api.openai.com/v1/chat",
 });
 
 const openai = new OpenAIApi(configuration);
