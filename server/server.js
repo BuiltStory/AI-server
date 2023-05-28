3ae5def1a58bcc43a5d0638af63bcd13a84960ac
Brian Alonso <bcalonso@builtstory.com>
2023-05-28 18:37:06 -0400

    Revert server back and try creatChatCompletion

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-  basePath: "https://api.openai.com/v1/chat",
 });
 
 const openai = new OpenAIApi(configuration);
@@ -26,17 +25,17 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({
+    const response = await openai.createChatCompletion({
       model: "gpt-3.5-turbo",
-      messages: prompt,
-      temperature: 0.5, // Higher values means the model will take more risks.
+      messages: 'S{prompt}',
+      temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 256, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      //frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      //presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
-    console.log(response.data);
+    //console.log(response.data);
 
     res.status(200).send({
       bot: response.data.choices[0].text
